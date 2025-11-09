23e604f1e00102e1084dbd8df03067d56dec9473
Anderson Guastuci <52762221+guastuci@users.noreply.github.com>
2025-11-09 17:21:46 -0300

    Add files via upload

---
+++ Controllers/TarefasController.cs
@@ -0,0 +1,71 @@
+﻿using GerenciadorDeTarefas.Contracts.Requests;
+using GerenciadorDeTarefas.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GerenciadorDeTarefas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefasController : ControllerBase
+    {
+        private readonly ITarefaService _service;
+
+        public TarefasController(ITarefaService service) => _service = service;
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CriarTarefaRequest request)
+        {
+            try
+            {
+                var created = _service.Create(request);
+                return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.GetAll());
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            var tarefa = _service.GetById(id);
+
+            if (tarefa == null)
+                return NotFound(); // 404
+
+            return Ok(tarefa); // 200
+        }
+
+
+        [HttpPut("{id:guid}")]
+        public IActionResult Update(Guid id, [FromBody] AtualizarTarefaRequest request)
+        {
+            try
+            {
+                var updated = _service.Update(id, request);
+                if (updated == null)
+                    return NotFound();
+
+                return Ok(updated);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult Delete(Guid id)
+        {
+            var ok = _service.Delete(id);
+            return ok ? NoContent() : NotFound();
+        }
+    }
+}
---
+++ Services/ITarefaService.cs
@@ -0,0 +1,16 @@
+﻿using GerenciadorDeTarefas.Contracts.Requests;
+using GerenciadorDeTarefas.Contracts.Responses;
+using System;
+using System.Collections.Generic;
+
+namespace GerenciadorDeTarefas.Services
+{
+    public interface ITarefaService
+    {
+        TarefaResponse Create(CriarTarefaRequest request);
+        IEnumerable<TarefaResponse> GetAll();
+        TarefaResponse? GetById(Guid id);
+        TarefaResponse? Update(Guid id, AtualizarTarefaRequest request);
+        bool Delete(Guid id);
+    }
+}
